cb7e8ee193c60241c67d001c0ae9fc3dbe4a60b7
Abinaya Ramachandran <abinayaksrv@gmail.com>
2021-01-27 11:45:26 -0800

    added api key

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "5IqDxZdZwtoB8H81ZRl7lyUM7cG2Xs5m";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
